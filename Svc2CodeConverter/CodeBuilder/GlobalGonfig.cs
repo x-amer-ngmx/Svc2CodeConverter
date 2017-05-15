f0da53064d74768358a06cf09e61eb281daaf62a
x-amer-ngmx <naguwal@gmail.com>
2017-05-15 14:03:44 +0300

    Корректировки GlobalConfig

--- Svc2CodeConverter/CodeBuilder/GlobalGonfig.cs
+++ Svc2CodeConverter/CodeBuilder/GlobalGonfig.cs
@@ -9,20 +9,26 @@ namespace Svc2CodeConverter.CodeBuilder
         public static readonly string SitPassword = ConfigurationManager.AppSettings["basic_password"] ?? "rZ_GG72XS^Vf55ZW";
         private static readonly string CurrentPlatform = ConfigurationManager.AppSettings["current_platform"];
         private static readonly string ServicePoint = ConfigurationManager.AppSettings[$"service_{CurrentPlatform}"];
-        private static readonly string ServicePointAddress = ConfigurationManager.AppSettings[$"servicepoint_{((CurrentPlatform != "file") ? "uri" : "file")}"];
+        private static readonly string ServicePointAddress = ConfigurationManager.AppSettings[$"servicepoint_{((CurrentPlatform != "file") ? "uri" : "patch")}"];
+        private static string[] _result;
 
         public static string[] EndPointAddress
         {
             get
             {
                 if (null != _result) return _result;
-                var mass = System.Text.RegularExpressions.Regex.Replace(ServicePointAddress, @"[\s\r\n]+", string.Empty).Split(',');
-                _result = mass.Select(x => $"{ServicePoint}{((CurrentPlatform != "file") ? @"/" : string.Empty)}{x}{((CurrentPlatform != "file") ? "?wsdl" : string.Empty)}").ToArray();
+                var mass = System.Text.RegularExpressions.Regex.Replace(
+                    ServicePointAddress,
+                    @"[\s\r\n]+",
+                    string.Empty).Split(',');
+
+                _result = mass.Select(
+                    x => $"{ServicePoint}{((CurrentPlatform != "file") ? @"/" : string.Empty)}{x}{((CurrentPlatform != "file") ? "?wsdl" : string.Empty)}").ToArray();
                 return _result;
             }
 
         }
 
-        private static string[] _result;
+        
     }
 }
